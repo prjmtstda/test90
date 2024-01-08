0afd1130cf48ba551d5a80687a2b3b814b4a2f49
QCT <snkta2018@gmail.com>
2024-01-08 18:53:45 +0530

    Commit was done.

---
+++ src/pkg/Manager_1.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class Manager_1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Testing");
+		System.out.println("QC Testing");
+		System.out.println("Database Testing");
+		System.out.println("Done1");
+		System.out.println("Codebase");
+		System.out.println("Done");
+	}
+}
